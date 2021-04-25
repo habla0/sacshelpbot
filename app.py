036cabe5c5e9e950f75f79b5982839b467fd0b27
Xavier <48151834+nemop0@users.noreply.github.com>
2021-04-26 03:20:20 +1000

    Added a test image

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 import re
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return '<h1 style="font-family: Comic Sans MS", sans-serif>0 bobux</h1>'
+    return '<h1 style="font-family: Comic Sans MS", sans-serif>0 bobux go to /drip</h1>'
+
+@app.route('/drip')
+def spicy():
+    return send_file("test_image.jpg")
 
 app.run(debug=True)
