3f2c9796c7ebfdbe0172f1a3a76d5a101de0ad78
Xavier <48151834+nemop0@users.noreply.github.com>
2021-04-27 21:36:06 +1000

    more modifications

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return '<h1 style="font-family: Comic Sans MS", sans-serif>0 bobux go to /drip</h1>'
+    return '<h1 style="font-family:Comic Sans MS;text-align:center;font-size:100px;">go to /drip or you have 0 bobux</h1>'
 
 @app.route('/drip')
 def spicy():
