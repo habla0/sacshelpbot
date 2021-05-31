72d470c9e79e4139d9083f8aac70da31cebb1262
Xavier <48151834+nemop0@users.noreply.github.com>
2021-05-31 14:58:30 +1000

    cleaned up app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 import re
 
 conversation = []
@@ -13,40 +13,29 @@ def homepage():
 @app.route('/bot', methods=["GET","POST"])
 def bot():
     global conversation
+
     responseDict = {
         "help": "Help msg",
         "homework": "do homework",
         "where": "ahhhh",
         "another": "beep"
     }
+
     if request.method == "GET":
-        print('get')
         return render_template('chat.html', conversation=conversation)
 
     if request.method == "POST":
         userInput = request.form.get("userInput")
         conversation.append(userInput)
 
-        # because im checking for every key, its going to spit many responses
+        botMsg = "default"
         for key in responseDict:
-            print('key:',key)
-            print('user:',userInput)
-
             if key in userInput:
                 botMsg = responseDict[key]
-                conversation.append(botMsg)
-                print('dictkey:',responseDict[key])
-                break # bad fix
-            else: 
-                botMsg = "default" # this is broken, will print until key in input
-                conversation.append(botMsg)
-                
-
-                
+        conversation.append(botMsg)
         
-            
         print(conversation)
         return render_template('chat.html', conversation=conversation)
-    return render_template('chat.html') # I'm sure this won't cause any problems later
+    #return render_template('chat.html') # I'm sure this won't cause any problems later
 
 app.run(debug=True)
